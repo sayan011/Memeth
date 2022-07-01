a34c5ae10d5db4fd61c71d718749cd2f93ea9f9f
Sayan Chowdhury <sayan.chowdhury.aiml25@heritageit.edu.in>
2022-07-02 03:04:03 +0530

    fixed landing page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f8681df04f81813d"; }
+/******/ 		__webpack_require__.h = function() { return "2ca088bbb9336ed1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
